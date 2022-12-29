b168d790e00e9a4b2e7b79c430c6a26e48f343db
Sachinmahajana <sachinmahajan782@gmail.com>
2022-12-29 13:26:54 +0530

    Added: Added Day6 & Day7

---
+++ Day6/primepallendrom.sh
@@ -0,0 +1,54 @@
+read -p "Enter the number: " number
+
+# VARIABLE
+originalNumber=$number
+reverse=0
+count=0
+
+# PRIME FUNCTION
+function isPrime()
+{
+	for (( i=1; i<=$number; i++ ))
+	do
+		if [ $(( number % i )) -eq 0 ]
+		then
+			count=$(( count + 1 ))
+		fi
+	done
+
+	if [ $count -eq 2 ]
+	then
+		echo $number is Prime Number
+
+		#TO PRINT FUNCTION 
+		echo "$( isPalindrome $number )"
+	else
+		echo $number is not prime Number
+	fi
+}
+
+#TO FUNCTION PALINDROME
+function isPalindrome()
+{
+	while [ $number -ne 0 ]
+	do
+		remainder=$(( $number % 10 ))
+		reverse=$(( reverse * 10 +remainder ))
+		number=$(( $number / 10 ))
+		 
+
+	done
+	echo $reverse
+
+	if [ $originalNumber -eq $reverse ]
+	then
+		echo "Number is Palindrome"
+	else
+		echo "Number is not Palindrome"
+	fi
+}
+
+# TO PRINT FUNCTION 
+echo "$( isPrime $number )"
+echo "$( isPrime $reverse )"
+
